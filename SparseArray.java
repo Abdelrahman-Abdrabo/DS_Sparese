73217532053c4413718d328db35f9e1e7e80d198
Abdrabo <abdrabo.elsagheer@gmail.com>
2023-01-31 13:37:02 +0200

    create Node class in SparseArray class

--- SparseArray.java
+++ SparseArray.java
@@ -1,3 +1,23 @@
-public class SparseArray {
+public class SparseArray<E> {
+    private static class Node<E> {
+        private Integer Element;
+        private Node<E> prev;
+        private Node<E> Next;
+        private int index ;
+        public Node(Integer e, Node<E> p, Node<E> n ,int index) {
+            this.index=index;
+            Element = e;
+            prev = p;
+            Next= n;
+        }
+        public int getIndex() {return index;}
+        public void setIndex(int index) {this.index = index;}
+        public Integer getElement() { return Element; }
+        public Node<E> getPrev() { return prev; }
+        public Node<E> getNext() { return Next; }
+        public void setPrev(Node<E> p) { prev = p; }
+        public void setNext(Node<E> n) { Next = n; }
+    }
+    
 
 }
