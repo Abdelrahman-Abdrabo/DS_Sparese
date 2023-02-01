b2645a200d4bcf944e5ede991a18d301d2bb4cca
Abdrabo <abdrabo.elsagheer@gmail.com>
2023-02-01 16:56:24 +0200

    test the sparse array class in main class

--- Main.java
+++ Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("-------------------Testing sparse array-------------------");
+        SparseArray<Integer> SA =new SparseArray<>(5);
+        SA.setValue(5,0);
+        SA.setValue(5,-6);
+        SA.setValue(0,1);
+        SA.setValue(6,2);
+        SA.setValue(7,3);
+        System.out.println("-----------------------------------------------------------");
+        SA.printAll();
+        System.out.println("-----------------------------------------------------------");
+        SA.printAllNoneZeros();
+        System.out.println("-----------------------------------------------------------");
+        SparseArray<Integer> SA2 =new SparseArray<>(5);
+        SA2.setValue(1,1);
+        SA2.setValue(2,2);
+        SA2.setValue(3,3);
+        SA2.setValue(4,4);
+        SA2.printAll();
+        System.out.println("-----------------------------------------------------------");
+        SA.add(SA2);
+        SA.printAll();
+        System.out.println("-----------------------------------------------------------");
+        SA.printAllNoneZeros();
+        System.out.println("-----------------------------------------------------------");
     }
 }
